eb311b508aa41884a3cf8850f356367657597f1d
AlbertoSONIC <albertosonic9000@gmail.com>
2014-10-25 18:56:28 +0200

    Almost everything fixed! Few framebuffer glitches..
    ..still there.

---
+++ source/program.c
@@ -0,0 +1,177 @@
+#include "program.h"
+#include "draw.h"
+
+//For ctrulib
+#include <3ds\types.h>
+#include <3ds\services\hid.h>
+#include <3ds\gfx.h>
+
+//For sprintF
+#include <stdio.h>
+
+int converted = 0;
+int bit = 0;
+int state = 0;
+int val[8];
+int valtoprint;
+int position = 1;
+char* A = "1";
+char* B = "0";
+u32 input;
+u8* screenBottom = 0;
+
+void variablereset()
+{
+	position = 1;
+	state = 0;
+	bit = 0;
+		converted = 0;
+		int ii;
+		for (ii = 0; ii<8; ii++)
+		{
+			val[ii] = 0;
+		}
+		input = 0;
+}
+
+
+void program()
+{
+	hidScanInput();
+	input = hidKeysDown();
+
+	//If first boot or time to reset
+	if (state == 0 || (state == 2 && input & KEY_UP))
+	{
+
+		screenBottom = gfxGetFramebuffer(GFX_BOTTOM, GFX_BOTTOM, NULL, NULL);
+		clearScreen(screenBottom, GFX_BOTTOM);
+		variablereset();
+		maintitle();
+		screenrender();
+		maintitle();
+		screenrender();
+		state = 1;
+	}
+
+	//If normal state, take input
+	if (bit < 8)
+	{
+		if (input & KEY_A)
+		{
+			valtoprint = 1;
+			val[bit] = 1;
+			bit++;
+			renderinput();
+		}
+		if (input & KEY_B)
+		{
+			valtoprint = 0;
+			val[bit] = 0;
+			bit++;
+			renderinput();
+		}
+		
+	}
+
+	//If bit limit reached, then it's time to convert
+	if (bit == 8 && state!=2)
+	{
+        conversion();
+		state = 2;
+	}
+
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+}
+
+//Screen render
+void screenrender()
+{
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+	screenBottom = gfxGetFramebuffer(GFX_BOTTOM, GFX_BOTTOM, NULL, NULL);
+}
+
+//Main screen
+void maintitle()
+{
+	clearScreen(screenBottom, GFX_BOTTOM);
+
+	char buffer[100];
+	sprintf(buffer, "***************************************");
+	drawString(buffer, 1, 1, 255, 255, 255, screenBottom, GFX_BOTTOM);
+
+	sprintf(buffer, "*                                     *");
+	drawString(buffer, 1, 11, 255, 255, 255, screenBottom, GFX_BOTTOM);
+
+	sprintf(buffer, "*     Binary -> Decimal Converter     *");
+	drawString(buffer, 1, 21, 255, 255, 255, screenBottom, GFX_BOTTOM);
+
+    sprintf(buffer, "*                                     *");
+	drawString(buffer, 1, 31, 255, 255, 255, screenBottom, GFX_BOTTOM);
+
+	sprintf(buffer, "*****  by AlbertoSONIC and Relys ******");
+	drawString(buffer, 1, 41, 255, 255, 255, screenBottom, GFX_BOTTOM);
+
+	sprintf(buffer, "Insert a binary number that you want to");
+	drawString(buffer, 1, 61, 255, 255, 255, screenBottom, GFX_BOTTOM);
+
+	sprintf(buffer, "convert (A=1, B=0, max 8bit)");
+	drawString(buffer, 1, 71, 255, 255, 255, screenBottom, GFX_BOTTOM);
+
+}
+
+
+//Input rendering
+void renderinput()
+{
+	printinput();
+	screenrender();
+	printinput();
+	screenrender();
+	position = position + 8;
+}
+
+//Input printing
+void printinput()
+{
+	if (valtoprint == 1)
+	{
+		drawString(A, position, 81, 255, 255, 255, screenBottom, GFX_BOTTOM);
+	}
+	if (valtoprint == 0)
+	{
+		drawString(B, position, 81, 255, 255, 255, screenBottom, GFX_BOTTOM);	
+	}
+	
+}
+
+
+//Conversion
+void conversion()
+{
+	converted += val[7] * 1 + val[6] * 2 + val[5] * 4 + val[4] * 8 + val[3] * 16 + val[2] * 32 + val[1] * 64 + val[0] * 128;
+	renderconversion();
+}
+
+void printconversion()
+{
+	char buffer[100];
+	
+	sprintf(buffer, "The result is %d", converted);
+	drawString(buffer, 1, 111, 255, 255, 255, screenBottom, GFX_BOTTOM);
+
+	sprintf(buffer, "Thank you for using this homebrew!");
+	drawString(buffer, 1, 131, 255, 255, 255, screenBottom, GFX_BOTTOM);
+
+	sprintf(buffer, "Press UP to restart this homebrew");
+	drawString(buffer, 1, 141, 255, 255, 255, screenBottom, GFX_BOTTOM);
+}
+void renderconversion()
+{
+	printconversion();
+	screenrender();
+	printconversion();
+	screenrender();
+}
---
+++ source/program.h
@@ -0,0 +1,14 @@
+#ifndef PROGRAM_H
+#define PROGRAM_H
+
+void maintitle();
+void screenrender();
+void program();
+void renderinput();
+void variablereset();
+void printinput();
+void conversion();
+void printconversion();
+void renderconversion();
+
+#endif
