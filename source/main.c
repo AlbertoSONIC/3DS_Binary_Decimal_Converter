8f72c3bc043eaa7cecf8138775cd574865227e20
AlbertoSONIC <albertosonic9000@gmail.com>
2014-12-05 16:29:07 +0100

    Updated ctrulib, now it's ready for ninjhax. Exit is not working.

--- source/main.c
+++ source/main.c
@@ -1,44 +1,22 @@
 #include "main.h"
 #include "program.h"
+
 /* New ctrulib */
 #include <3ds.h>
 int main()
 {
-	//Grab the status of the app (running, suspending, sleeping, exiting)
-	APP_STATUS status;
-
 	//Initialize 3ds services
 	srvInit();
 	fsInit();
 	aptInit();
 	gfxInit();
 	hidInit(NULL);
-	//Setup event handler to listen for changes in status
-	aptSetupEventHandler();
 
-
-	while ((status = aptGetStatus()) != APP_EXITING)
+	// Main loop
+	while (aptMainLoop())
 	{
-
-		if (status == APP_RUNNING)
-		{
-			//If the app is currently in the forground running, execute the program.
-			program();
-		}
-		else if (status == APP_SUSPENDING)
-		{
-			//If the app is currently suspended in the background, return to the home menu.
-			aptReturnToMenu();
-		}
-		else if (status == APP_SLEEPMODE)
-		{
-			//If the app is currently suspended in sleep mode, wait.
-			aptWaitStatusEvent();
-		}
-
-		//Wait for GPU
 		gspWaitForVBlank();
-
+		program();
 	}
 
 	//Close all opened services.
